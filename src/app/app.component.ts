d9bdd40549a50c225c3cbcf6a8643ac982ea5553
rajckamariya <r.c.kamariya@gmail.com>
2021-08-27 12:24:47 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component, VERSION } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
